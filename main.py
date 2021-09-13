bc0d0fbf7714f38e2192f9bef16d5e89fd85bd0a
Udyan Sharma <udyan2@gmail.com>
2021-09-13 14:22:33 +0530

    test

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello World")
+#variable
+import record
+import pandas as pd
+
+import_file_path="variables.xlsx"
+voice_input=record.recorder().lower()
+print(voice_input)
+df=pd.read_excel(import_file_path)
+print(df)
+variables_list=df['Var_Declaration'].tolist()
+print(type(variables_list[0]))
+for element in variables_list:
+    ele=element.lower()
+    print("Variable command detected")
+    if "variable" in ele:
+        
+        if "declare a variable" in voice_input:
+            print(voice_input[19:])
+            lst=voice_input.split()
+            print(lst)
+            var_ind=lst.index("variable")
+            type_ind=lst.index("type")
+            command=lst[type_ind+1]+" "+lst[var_ind+1]
+        print("Variable command detected")
+    else:
+        print("Nothing related to variable")
+    
+print("Code Block: ")
+print(command)
---
+++ record.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 22:42:42 2021
+
+@author: Udyan Sharma
+"""
+import speech_recognition as sr
+import pyttsx3
+r=sr.Recognizer()
+eng = pyttsx3.init()
+
+def recorder():
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        voice_in=''
+        try:
+            voice_in=r.recognize_google(audio)
+            print("You:",voice_in)
+        except sr.UnknownValueError:
+            print("Xceleron: Sorry, I did not get that.")
+            eng.say("Sorry, I did not get that.")
+            eng.runAndWait()
+        except sr.WaitTimeoutError:
+            print("Wait timeout error.")
+            eng.say("Wait timeout error.")
+            eng.runAndWait()
+        except sr.RequestError:
+            print("Sorry, the service is currently down.")
+            eng.say("Sorry, the service is currently down.")
+            eng.runAndWait()
+        return voice_in
