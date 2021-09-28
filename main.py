5d7fe9b07143cb4424261922a4023f1f3605216d
Udyan Sharma <udyan2@gmail.com>
2021-09-29 01:05:34 +0530

    Update

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ voice_input=record.recorder().lower()
 print(voice_input)
 df=pd.read_excel(import_file_path)
 print(df)
-variables_list=df['Var_Declaration'].tolist()
-print(type(variables_list[0]))
-for element in variables_list:
+variable_commands_list=df['Var_Declaration'].tolist()
+print(type(variable_commands_list[0]))
+for element in variable_commands_list:
     ele=element.lower()
     print("Variable command detected")
     if "variable" in ele:
-        
-        if "declare a variable" in voice_input:
+        if "initialize a variable" in voice_input:
             print(voice_input[19:])
-            lst=voice_input.split()
+            input_list=voice_input.split()
             print(lst)
-            var_ind=lst.index("variable")
-            type_ind=lst.index("type")
-            command=lst[type_ind+1]+" "+lst[var_ind+1]
+            var_ind=input_list.index("variable")
+            type_ind=input_list.index("type")
+            val_ind=
+            var_name=input_list[type_ind+1]+" "+input_lst[var_ind+1]
+            
         print("Variable command detected")
     else:
         print("Nothing related to variable")
     
 print("Code Block: ")
+
 print(command)
+
